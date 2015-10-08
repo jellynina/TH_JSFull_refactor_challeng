435b40152ddb68093d7904c96a41ae4d5465f16c
jellynina <jellynina@gmail.com>
2015-10-09 05:21:01 +0800

    refactor with module functions.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,36 @@
 var html = '';
-var red;
-var green;
-var blue;
+var redColor;
+var greenColor;
+var blueColor;
 var rgbColor;
+
+function getRandomColorValue(){
+	return Math.floor(Math.random() * 256);
+}
+
+function getRandomRGB (){
+	var theRGB;
+	theRGB = 'rgb(';
+	for (var i = 0; i < 3; i++) {
+		theRGB += getRandomColorValue();
+		if (i != 2) {
+			theRGB += ',';
+		};
+	};
+	theRGB +=')';
+	
+	console.log(theRGB);
+
+	return theRGB;
+}
 
 
 for (var i = 0; i < 10; i++) {
-	red = Math.floor(Math.random() * 256 );
-	green = Math.floor(Math.random() * 256 );
-	blue = Math.floor(Math.random() * 256 );
-	rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+	// redColor = Math.floor(Math.random() * 256 );
+	// greenColor = Math.floor(Math.random() * 256 );
+	// blueColor = Math.floor(Math.random() * 256 );
+	// rgbColor = 'rgb(' + redColor + ',' + greenColor + ',' + blueColor + ')';
+	rgbColor = getRandomRGB();
 	html += '<div style="background-color:' + rgbColor + '"></div>';
 };
 
